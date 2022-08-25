acbb3d32035b30cb07954bb34b34c71a8e8eb951
Davi Serrano <daviscardoso2901@gmail.com>
2022-08-25 19:13:57 -0300

    salvo

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -2,9 +2,17 @@ import { GetServerSideProps } from "next";
 import {  Flex, Text, NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper ,Input, Image,  Stack,  Radio, RadioGroup } from '@chakra-ui/react'
 import { fauna } from "../services/fauna";
 import { query as q} from "faunadb"
+import { useEffect, useState } from "react";
+import { parseJSON } from "date-fns";
 
-export default function User(){
+export default function User(users: any){
+  const [a, setA] = useState()
 
+  useEffect(()=>{
+    setA(users)
+  }, [])
+
+  console.log(a)
     return(
 
       <Flex>
@@ -14,12 +22,21 @@ export default function User(){
 
 export const getServerSideProps:GetServerSideProps = async ()=>{
 
- 
+  const users:any = await fauna.query(
+    q.Map(
+      q.Paginate(q.Documents(q.Collection('users'))),
+      q.Lambda((show) => q.Get(show))
+    )
+    )
+    
+      console.log(users.data[0].data.email)
+      console.log(users.data[1].data.email)
+
 
 
   return {
     props:{
-
+      users: users.data[1].data.email
     }
   }
 }
